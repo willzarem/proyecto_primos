42c1f9b6c1f90a8370db7c82091a4c7cf321d309
Willmer Hernandez <willzarem@Willmers-MacBook-Pro.local>
2020-10-11 20:42:57 -0600

    REacord 5 seconds demo@

--- main.py
+++ main.py
@@ -5,6 +5,50 @@ import aubio
 
 p = pyaudio.PyAudio()
 
+output = "./output/1.wav"
+
 buffer_size = 1024
 pyaudio_format = pyaudio.paFloat32
-n_channels =
+n_channels = 1
+samplerate = 44100
+stream = p.open(format=pyaudio_format, channels=n_channels, rate=samplerate, input=True, frames_per_buffer=buffer_size)
+
+output_filename = output
+record_duration = 5
+outputsink = aubio.sink(output, samplerate)
+total_frames = 0
+
+tolerance = 0.8
+win_s = 4096
+hop_s = buffer_size
+pitch_o = aubio.pitch("default", win_s, hop_s, samplerate)
+pitch_o.set_unit("midi")
+pitch_o.set_tolerance(tolerance)
+
+print("*** starting recording")
+while True:
+    try:
+        audiobuffer = stream.read(buffer_size)
+        signal = np.fromstring(audiobuffer, dtype=np.float32)
+
+        pitch = pitch_o(signal)[0]
+        confidence = pitch_o.get_confidence()
+
+        print("{} / {}".format(pitch, confidence))
+
+        if outputsink:
+            outputsink(signal, len(signal))
+
+        if record_duration:
+            total_frames += len(signal)
+            if record_duration * samplerate < total_frames:
+                break
+    except KeyboardInterrupt:
+        print("*** Stopped")
+        break
+
+
+print("*** done recording")
+stream.stop_stream()
+stream.close()
+p.terminate()
