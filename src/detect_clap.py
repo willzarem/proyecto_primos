02ad4d52d38df9432f97f7b86e80939e42278bd6
Diana Hernandez <diher@mit.edu>
2020-10-12 13:36:11 -0400

    made detect clap parameters less arbitrary, added in cli args, created location to put sound sample files

--- src/detect_clap.py
+++ src/detect_clap.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import wave
+import sys
 from scipy.signal import find_peaks
 
-spf = wave.open("clapping2.wav", "r")
+filename = sys.argv[1]
+spf = wave.open(filename, "r")
 
 signal = spf.readframes(-1)
 signal = np.fromstring(signal, "Int16")
@@ -12,15 +14,18 @@ fs = spf.getframerate()
 Time = np.linspace(0, len(signal) / fs, num=len(signal))
 
 
-PEAK_DELTA = 10000
+PEAK_DELTA = max(signal)/2
 A = signal
 peaks, _ = find_peaks(A, distance=PEAK_DELTA)
 
 print(peaks)
 
+mean = np.mean(A, axis=0)
+sd = np.std(A, axis=0)
+
 final_peaks = []
 for peak in peaks:
-    if A[peak] > 3000:
+    if A[peak] > (mean + 6*sd):
         final_peaks.append(peak)
         print("clap", peak, A[peak])
 plt.plot(A)
