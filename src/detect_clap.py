2a6728f108b671c732e998ed4f7982ac3aaa20f7
Diana Hernandez <diher@mit.edu>
2020-10-14 15:52:15 -0400

    revamped clap detection code to be more modular

--- src/detect_clap.py
+++ src/detect_clap.py
@@ -4,30 +4,43 @@ import wave
 import sys
 from scipy.signal import find_peaks
 
-filename = sys.argv[1]
-spf = wave.open(filename, "r")
+class Clap:
+    def __init__(self):
+        filename = sys.argv[1]
+        spf = wave.open(filename, "r")
+        signal = spf.readframes(-1)
+        self.signal = np.fromstring(signal, "Int16")
+        self.fs = spf.getframerate()
+        self.peaks = np.array([])
+        self.final_peaks = []
+    
+    def detectClap(self):
 
-signal = spf.readframes(-1)
-signal = np.fromstring(signal, "Int16")
-fs = spf.getframerate()
+        Time = np.linspace(0, len(self.signal) / self.fs, num=len(self.signal))
 
-Time = np.linspace(0, len(signal) / fs, num=len(signal))
+        PEAK_DELTA = max(self.signal)/2
+        A = self.signal
+        peaks, _ = find_peaks(A, distance=PEAK_DELTA)
+        self.peaks = peaks;   
+        #print(peaks)
 
+        mean = np.mean(A, axis=0)
+        sd = np.std(A, axis=0)
 
-PEAK_DELTA = max(signal)/2
-A = signal
-peaks, _ = find_peaks(A, distance=PEAK_DELTA)
+        final_peaks = []
+        for peak in peaks:
+            if A[peak] > (mean + 6*sd):
+                final_peaks.append(peak)
+                print("clap", peak, A[peak])
+        self.final_peaks = final_peaks        
+        return final_peaks
 
-print(peaks)
+    def printPeaks(self):
+        A = self.signal
+        plt.plot(A)
+        plt.plot(self.peaks, A[self.peaks], "x")
+        plt.show()
 
-mean = np.mean(A, axis=0)
-sd = np.std(A, axis=0)
-
-final_peaks = []
-for peak in peaks:
-    if A[peak] > (mean + 6*sd):
-        final_peaks.append(peak)
-        print("clap", peak, A[peak])
-plt.plot(A)
-plt.plot(peaks, A[peaks], "x")
-plt.show()
+clap = Clap()
+print(clap.detectClap())
+clap.printPeaks()
