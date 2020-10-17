f992757eacccf049dd31f4b5563eaa57a0d0d33a
Diana Hernandez <diher@mit.edu>
2020-10-17 17:44:03 -0400

    got detect_clap functional again

--- src/detect_clap.py
+++ src/detect_clap.py
@@ -3,15 +3,20 @@ import numpy as np
 import wave
 import sys
 from scipy.signal import find_peaks
+import sys
+sys.path.append('/Users/diher/git/proyecto_primos/sound_samples')
 
 class Clap:
     def __init__(self, file):
-        filename = file
-        spf = wave.open(filename, "r")
-        signal = spf.readframes(-1)
-        self.signal = np.fromstring(signal, "Int16")
-        self.fs = spf.getframerate()
-        self.peaks = np.array([])
+        if(file == 'continuous'):
+            self.signal = os.p
+        else:
+            filename = file
+            spf = wave.open(filename, "r")
+            signal = spf.readframes(-1)
+            self.signal = np.fromstring(signal, "Int16")
+            self.fs = spf.getframerate()
+        self.peaks = []
         self.final_peaks = []
     
     def detectClap(self):
@@ -22,7 +27,6 @@ class Clap:
         A = self.signal
         peaks, _ = find_peaks(A, distance=PEAK_DELTA)
         self.peaks = peaks;   
-        #print(peaks)
 
         mean = np.mean(A, axis=0)
         sd = np.std(A, axis=0)
@@ -41,6 +45,6 @@ class Clap:
         plt.plot(self.peaks, A[self.peaks], "x")
         plt.show()
 
-clap = Clap()
+clap = Clap(sys.argv[1])
 print(clap.detectClap())
 clap.printPeaks()
